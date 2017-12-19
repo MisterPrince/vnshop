5a564278f85bd87c33d64de47c913020f830b14b
MisterPrince <wangzhiqiang1359@163.com>
2017-12-19 17:42:48 +0800

    logo css

--- x-shop-master/client/src/components/NavHeader.vue
+++ x-shop-master/client/src/components/NavHeader.vue
@@ -23,7 +23,7 @@
     <div class="navbar">
       <div class="navbar-left-container">
         <a href="/">
-          <img class="navbar-brand-logo" src="/static/logo.png">
+          <img class="navbar-brand-logo" src="/static/logo.jpg">
         </a>
       </div>
       <div class="navbar-right-container" style="display: flex;">
@@ -31,7 +31,7 @@
           <span class="navbar-link" v-text="nickName" v-if="nickName"></span>
           <router-link to="/orders" v-if="nickName">我的订单</router-link>
           <a href="javascript:void(0)" class="navbar-link" @click="registerModalFlag=true" v-if="!nickName">注册</a>
-          <a href="javascript:void(0)" class="navbar-link" @click="loginModalFlag=true" v-if="!nickName">hehe登录</a>
+          <a href="javascript:void(0)" class="navbar-link" @click="loginModalFlag=true" v-if="!nickName">登录</a>
           <a href="javascript:void(0)" class="navbar-link" @click="logOut" v-else>退出</a>
           <div class="navbar-cart-container">
             <span class="navbar-cart-count" v-text="cartCount" v-if="cartCount"></span>
